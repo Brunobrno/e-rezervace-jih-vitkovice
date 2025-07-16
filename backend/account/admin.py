6f63ec72bd80e492839d555f31f1b94fe5782c9d
Morddy <dubnickyd@gmail.com>
2025-07-16 10:54:57 +0200

    Update admin.py

--- backend/account/admin.py
+++ backend/account/admin.py
@@ -22,7 +22,7 @@ class CustomUserAdmin(UserAdmin):
 
     fieldsets = (
         (None, {"fields": ("username", "email", "password")}),
-        ("Osobní údaje", {"fields": ("role", "account_type", "phone_number", "var_symbol", "bank_acc", "ICO", "city", "street", "PSC")}),
+        ("Osobní údaje", {"fields": ("role", "account_type", "phone_number", "var_symbol", "bank_account", "ICO", "city", "street", "PSC")}),
         ("Práva a stav", {"fields": ("is_active", "is_staff", "is_superuser", "email_verified", "groups", "user_permissions")}),
         ("Důležité časy", {"fields": ("last_login",)}),  # create_time vyjmuto odsud
     )
@@ -77,7 +77,7 @@ class CustomUserAdmin(UserAdmin):
         if request.user.role == "cityClerk":
             return (
                 (None, {"fields": ("email", "username", "password")}),
-                ("Osobní údaje", {"fields": ("role", "account_type", "phone_number", "var_symbol", "bank_acc", "ICO", "city", "street", "PSC")}),
+                ("Osobní údaje", {"fields": ("role", "account_type", "phone_number", "var_symbol", "bank_account", "ICO", "city", "street", "PSC")}),
             )
         
         # Default for other users
