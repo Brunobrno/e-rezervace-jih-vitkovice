2b99b008c1dbfa44546e7b4416a83af80cc1d3fc
Morddy <dubnickyd@gmail.com>
2025-07-18 10:16:07 +0200

    .

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,7 @@ import '/node_modules/react-grid-layout/css/styles.css'
 import '/node_modules/react-resizable/css/styles.css'
 import { Routes, Route } from 'react-router-dom';
 import NavBar from './components/NavBar';
-import Login from './pages/ClerkHome';
+import Login from './pages/Login';
 import Register from './pages/Register';
 import CreateReservation from './pages/CreateReservation';
 import SelectReservation from './pages/SelectReservation';
